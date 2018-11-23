ae0c2119fb90a0b79fe1b8b580c1aa999fe66170
G.Ilshat <gaifullin.ilshat@gmail.com>
2018-11-23 12:24:36 +0700

    test

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
 //run is background
+var CACHE_NAME = 'todo-app-cache-v1';
+var urlsToCache = [
+    '/',
+    '/styles.css',
+    '/script.js',
+    '/img/feather-sprite.svg'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
